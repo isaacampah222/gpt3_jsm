c3897a63de7fb81b91262753c6bfd520262b828b
black-rush <caasihapma@gmail.com>
2023-01-02 12:01:29 +0000

    adding app css and refactoring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+import './App.css';
 
 const App = () => {
   return (
-    <div>GPT-3</div>
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      < CTA />
+      < Blog />
+      <Footer />
+    </div>
   )
 }
 
